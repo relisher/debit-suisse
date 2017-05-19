809803a76b376417a7d466e992c3028c92a0d09e
Jackson Abascal <jacksonabascal@gmail.com>
2017-05-19 03:40:19 -0400

    model fix

--- src/main/java/debitsuisse/projectfour/model.java
+++ src/main/java/debitsuisse/projectfour/model.java
@@ -115,12 +115,11 @@ public class model {
 
     //the return value of a specific weighting c
 	private double weightingReturn(double[] c) {
-		double ret = 0, w = 1;
-		for(int i = 0; i < companies-1; ++i) {
-			w -= c[i];
+		double ret = 0;
+		for(int i = 0; i < companies; ++i) {
 			ret += annual_avg_return[i]*c[i];
 		}
-		return ret+w*annual_avg_return[companies-1];
+		return ret;
 	}
 
 	//does the weighting fit the constraints?
